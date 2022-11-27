cefbc7e444087053afc3df74459f70152e366e8b
Elesdes <53079743+Elesdes@users.noreply.github.com>
2022-11-27 14:57:14 +0100

    MLP 2.0

--- src/libs/utils/tensorboard.py
+++ src/libs/utils/tensorboard.py
@@ -1,5 +1,6 @@
 import os
 from datetime import date
+from functools import wraps
 
 from src.libs.utils.default import DefaultParameters
 
@@ -11,29 +12,31 @@ def create_tensorboard_log_dir(type_of_algorithm: str = "Test",
     || Every parameter are set by default to "Test"."""
 
     def decorator(function):
+        @wraps(function)
         def wrapper(path: str = None, *args):
-            # Relative path from this file to tensorboard log directory
-            base_dir_logs = "../../../tensorboard_logs"
+            if path is None:
+                # Relative path from this file to tensorboard log directory
+                base_dir_logs = "../../../tensorboard_logs"
 
-            # Create a path file name with date and other variables
-            experiment_dir_logs = f"{date.today().strftime('%d%m%Y')}" \
-                                  f"_bs_{DefaultParameters.batch_size}" \
-                                  f"_lr_{DefaultParameters.learning_rate}" \
-                                  f"_mom_{DefaultParameters.momentum}" \
-                                  f"_hl_{DefaultParameters.num_hidden_layers}" \
-                                  f"_nphl_{DefaultParameters.num_neurons_per_hidden_layers}"
+                # Create a path file name with date and other variables
+                experiment_dir_logs = f"{date.today().strftime('%d%m%Y')}" \
+                                      f"_bs_{DefaultParameters.batch_size}" \
+                                      f"_lr_{DefaultParameters.learning_rate}" \
+                                      f"_mom_{DefaultParameters.momentum}" \
+                                      f"_hl_{DefaultParameters.num_hidden_layers}" \
+                                      f"_nphl_{DefaultParameters.num_neurons_per_hidden_layers}"
 
-            paths = [f"{base_dir_logs}",
-                     f"{base_dir_logs}/{type_of_algorithm}",
-                     f"{base_dir_logs}/{type_of_algorithm}/{algorithm_used}",
-                     f"{base_dir_logs}/{type_of_algorithm}/{algorithm_used}/{experiment_dir_logs}"]
+                dirs = [f"{base_dir_logs}",
+                         f"{base_dir_logs}/{type_of_algorithm}",
+                         f"{base_dir_logs}/{type_of_algorithm}/{algorithm_used}",
+                         f"{base_dir_logs}/{type_of_algorithm}/{algorithm_used}/{experiment_dir_logs}"]
 
-            # Creating every dirs that doesn't exist yet
-            for path in paths:
-                if not os.path.isdir(path):
-                    os.mkdir(path)
+                # Creating every dirs that doesn't exist yet
+                for dir in dirs:
+                    if not os.path.isdir(dir):
+                        os.mkdir(dir)
 
-            return function(path=paths[3], *args)
+            return function(path=dirs[3], *args)
 
         return wrapper
 
